b2ae042b7c7b7e6dc21c047f555bd6a3051a7016
Laiot <carmelo.sarta.main@gmail.com>
2022-07-12 16:01:13 +0200

    Enabled Prometheus metrics

--- src/main/java/dev/snowdrop/example/ExampleApplication.java
+++ src/main/java/dev/snowdrop/example/ExampleApplication.java
@@ -21,6 +21,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnablePrometheusMetrics
 public class ExampleApplication {
 
     public static void main(String[] args) {
